29114c3527aae439088c723f0c7b80e4d97e8e90
nvsang260720 <nvsang.2670@gmail.com>
2023-02-20 22:16:50 +0700

    fix code

--- db/myapp.sql
+++ db/myapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2023 at 04:07 PM
+-- Generation Time: Feb 20, 2023 at 04:16 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `myapp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`id`, `email`, `password`, `phone`) VALUES
+(1, 'admin1@gmail.com', 's123456', 123456),
+(2, 'admin@gmail.com', 's123456', 123456),
+(3, 'fdsf', 'sdfds', NULL),
+(4, 'nvsang.18it2@vku.udn.vn', '123456789', NULL),
+(5, 'nvsang.18it2@vku.udn.vn', '12345', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(255) NOT NULL,
+  `banner` varchar(255) NOT NULL,
+  `tilte` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `banner`, `tilte`) VALUES
+(1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm0UtZPe2ttsTRyLUK6kyWSzPLCf5mDPyPjQ&usqp=CAU', 'category 1'),
+(2, 'https://phongcachviettravel.vn/wp-content/uploads/2022/12/slide-du-lich-hanh-huong.png', 'category 2'),
+(3, 'https://phongcachviettravel.vn/wp-content/uploads/2022/12/slide-du-lich-hanh-huong.png', 'categori 24'),
+(4, 'https://phongcachviettravel.vn/wp-content/uploads/2022/12/slide-du-lich-hanh-huong.png', 'categori 24');
 
 -- --------------------------------------------------------
 
@@ -48,6 +94,18 @@ INSERT INTO `products` (`id`, `banner`, `address`, `title`, `price`) VALUES
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -56,6 +114,18 @@ ALTER TABLE `products`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
